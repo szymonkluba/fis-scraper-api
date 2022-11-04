052ae1bf5f2881450c034d5286d8220fcb4382fd
szymonkluba <szymonkluba@gmail.com>
2022-11-05 00:36:38 +0100

    Disabled debug mode

--- fis_scraper_backend/settings.py
+++ fis_scraper_backend/settings.py
@@ -28,7 +28,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "fisscraper.pythonanywhere.com"
